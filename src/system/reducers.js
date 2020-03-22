b1768e6ba73e9d91ceff3512423babe4d148668b
kantapong00 <kantapong_kasemsan@elearning.cmu.ac.th>
2020-03-22 14:08:32 +0700

    delete file not use

--- src/system/reducers.js
+++ src/system/reducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
-import todoReducer from './todoReducer'
 
 const reducers = history =>
   combineReducers({
-    todos: todoReducer,
     router: connectRouter(history)
   });
 
